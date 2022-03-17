5746177155f0bfaa8471c8e00f11b06a253c8c92
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-17 11:11:13 +0300

    replace remove() by .active

--- script/app.js
+++ script/app.js
@@ -1,4 +1,11 @@
 // every book isInstance of a book class;
+let body = null;
+let footer = null;
+let nav = null;
+let addBook = null;
+let ListOfBooks = null;
+let contactInfo = null;
+
 class MyBook {
   static listOfBook = [];
 
@@ -74,84 +81,40 @@ const addNewBookEvent = () => {
     event.preventDefault();
     const title = event.target.elements[0].value;
     const author = event.target.elements[1].value;
-    if (title !== '') {
-      const myNewBook = new MyBook(title, author);
-      MyBook.unRender.push(myNewBook);
+    const myNewBook = new MyBook(title, author);
+    myNewBook.addBookToDom();
       event.target.elements[0].value = '';
-      event.target.elements[1].value = '';
-    }
+    event.target.elements[1].value = '';
   });
 };
+
+const swapSection = (newActiveSection) => {
+  const oldActiveSection = document.querySelector('section.active');
+  oldActiveSection.classList.toggle('active');
+  newActiveSection.classList.add('active');
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   if (!localStorage.getItem('bookCollection')) {
     localStorage.setItem('bookCollection', JSON.stringify([]));
   } else {
     updateBookList();
   }
-});
-
-let body = null;
-let footer = null;
-let nav = null;
-let addBook = null;
-let ListOfBooks = null;
-let contactInfo = null;
-const swapSection = (section) => {
-  contactInfo.remove();
-  ListOfBooks.remove();
-  addBook.remove();
-  body.insertBefore(section, footer);
-  if (section === addBook) {
-    addNewBookEvent();
-  } else if (section === ListOfBooks && MyBook.unRender.length > 0) {
-    MyBook.unRender.forEach((book) => {
-      book.addBookToDom();
-    });
-    MyBook.unRender = [];
-  }
-};
-
-const getNewBookSection = () => {
-  const newBookSection = document.createElement('section');
-  newBookSection.setAttribute('id', 'new-book-section');
-  newBookSection.innerHTML = `
-    <h2 class="new-book-text"> Add a New Book</h2>
-    <form action="#" id="newBook">
-      <input type="text" id="title" name="title" placeholder="Title" required minlength="2" maxlength="100">
-        <input type="text" id="author" name="author" placeholder="Author" required minlength="2" maxlength="100">
-          <button type="submit" id="addBook">Add</button>
-        </form>`;
-  return newBookSection;
-};
-
-const getContactInfo = () => {
-  const contactInfoSection = document.createElement('section');
-  contactInfoSection.setAttribute('id', 'contact-info');
-  contactInfoSection.innerHTML = `
-<h2 id="contact-info-title">Contact Information</h2>
-<p>Do you have any questions or you just want to say "Hello"? <br> You can reach out to us!</p>
-<ul>
-  <li>Our e-mail: email24t@gmail.com</li>
-  <li>Our phone number: +251921577930</li>
-  <li>Our address: Streetname 22, 84503 City, Country</li>
-</ul>
-`;
-  return contactInfoSection;
-};
-
-document.addEventListener('DOMContentLoaded', () => {
   body = document.querySelector('body');
   footer = document.getElementById('footer-container');
   nav = document.getElementById('navbar-container');
   ListOfBooks = document.getElementById('list-of-books');
-  addBook = getNewBookSection();
-  contactInfo = getContactInfo();
+  addBook = document.getElementById('new-book-section');
+  contactInfo = document.getElementById('footer-container');
   const navLinks = nav.querySelectorAll('a');
+  ListOfBooks.classList.add('active');
+  addNewBookEvent();
   function swapSectionEvent(element) {
     element.addEventListener('click', (event) => {
       event.preventDefault();
       const sectionName = element.getAttribute('href').replace('#', '');
-      if (sectionName === 'ListOfBooks') { swapSection(ListOfBooks); } else if (sectionName === 'addBook') { swapSection(addBook); } else { swapSection(contactInfo); }
+      console.log(sectionName);
+      swapSection(document.getElementById(sectionName));
     });
   }
   for (let i = 0; i < navLinks.length; i += 1) {
